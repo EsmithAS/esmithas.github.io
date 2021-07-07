03f24a25b169967bd7346788aae887f4f16528a5
EsmithAS <esmith_as@hotmail.com>
2021-07-06 21:09:54 -0500

    Se agregó loading y cambio de tema automático

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,19 @@
 const qs = document.querySelector.bind(document);
+const qsAll = document.querySelectorAll.bind(document);
 
+/*----------------
+-- JS LOADING
+------------------*/
+window.onload = ()=>{
+    setTimeout(() => {
+        qs('.loading').classList.add('hidden-element');
+        qs('html').classList.remove('overflow-y-hidden')
+    }, 3000);
+}
+
+/*----------------
+-- JS SHOW MENU
+------------------*/
 qs('#btn-menu').addEventListener( 'click' , (e) => {
 
     let icon = 'assets/svg/icon/menu.svg#menu';
@@ -14,8 +28,19 @@ qs('#btn-menu').addEventListener( 'click' , (e) => {
 
 } );
 
-qs('#btn-theme').addEventListener( 'click' , (e) => {
+/*------------------------
+-- JS THEME DARK - LIGHT
+--------------------------*/
+const date = new Date();
 
+if( date.getHours() >= 19 ) {
+    changeTheme();
+}
+qs('#btn-theme').addEventListener( 'click' , (e) => {
+    changeTheme();    
+} );
+
+function changeTheme () {
     let icon = 'assets/svg/icon/moon.svg#moon';
 
     qs('body').classList.toggle('dark');
@@ -25,8 +50,10 @@ qs('#btn-theme').addEventListener( 'click' , (e) => {
     }
 
     qs('#btn-theme > svg > use').setAttribute( 'href', icon );
-} );
-
+}
+/*----------------
+-- JS SWIPER
+------------------*/
 const Swipes = new Swiper('.swiper-container', {
     loop: false,
     navigation: {
@@ -37,3 +64,28 @@ const Swipes = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
     },
 });
+
+/*----------------
+-- JS SELECT MENU
+------------------*/
+qs('header nav').addEventListener( 'click' , (e)=> {
+    if (e.target.nodeName === 'A') {
+        qs('a.selected').classList.remove('selected')
+        e.target.classList.add('selected')
+    }
+} )
+const sections = {};
+qsAll('section').forEach( e =>{
+    sections[e.id] = e.offsetTop;
+})
+window.onscroll = ()=>{
+    const scrollY = window.scrollY
+    for( let i in sections ){
+        if (sections[i] <= scrollY) {
+            if (qs('nav a[href*='+ i +']') && qs('a.selected')) {
+                qs('a.selected').classList.remove('selected')
+                qs('nav a[href*='+ i +']').classList.add('selected')
+            }
+        }
+    }
+}
